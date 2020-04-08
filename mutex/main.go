e189e82d7fcf319bc65cd0c15e477b3ecfce283b
Parham Alvani <parham.alvani@gmail.com>
2020-04-08 15:48:53 +0430

    feat: Add Mutex Example

---
+++ mutex/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+const N = 10000
+
+func main() {
+	sum := 0
+
+	m := New()
+	d := make(chan string)
+
+	for i := 0; i < N; i++ {
+		go func(mutex Mutex, done chan string) {
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			sum++
+			if sum == N {
+				done <- "done"
+			}
+		}(m, d)
+	}
+
+	<-d
+
+	fmt.Println(sum)
+}
+
+type Mutex struct {
+	l chan string
+}
+
+func New() Mutex {
+	return Mutex{l: make(chan string, 1)}
+}
+func (m Mutex) Lock() {
+	m.l <- "lock"
+}
+
+func (m Mutex) Unlock() {
+	<-m.l
+}
